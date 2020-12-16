e15235a2419d4ac0317e0e4e25bf500464eb4822
J Rhee <jayrhee0514@gmail.com>
2020-12-16 15:31:41 -0500

    fixed local storage and added an extra id to see if that helps the layout of the page

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,55 @@
 //add the date under h1
-$(document).ready(function(){
+$(document).ready(function () {
     var currentDate = moment().format('dddd MMMM Do');
     $('#currentDay').text(currentDate);
 
     //adding the colors for specfic time 
-    function timeZone () {
-        var eventTime = moment().hour();
-        console.log(eventTime);
+    function timeZone() {
+        var currentHour = moment().hour();
         var currentTime = $('.row');
         //convert military time to standard time
-        currentTime.each(function(){
+        currentTime.each(function () {
             var match = parseInt($(this).attr('id'))
-            if (match < 9){
-                match +=12;
+            if (match < 9) {
+                match += 12;
             }
-            console.log(eventTime, match);
+            console.log(currentHour, match);
             let time = $(this)[0];
-            if (eventTime === match){
+            if (currentHour === match) {
                 $(time).addClass('present')
-            } else if (eventTime > match) {
+            } else if (currentHour > match) {
                 $(time).addClass('past')
-            } else if (eventTime < match) {
+            } else if (currentHour < match) {
                 $(time).addClass('future')
             }
-        })
+        });
     }
     timeZone();
 
     //saving data to local storage
-    function saveEvent(event){
+    function saveEvent(event) {
         event.preventDefault();
 
         var value = $(this).prev().val();
-        console.log(value);
+        // console.log(value);
         var time = $(this).parent().attr("id");
-        console.log(time);
+        // console.log(time);
 
-        localStorage.setItem(time, value)
+        localStorage.setItem(time, value);
     };
 
     $(".saveBtn").click(saveEvent);
 
     //loop through local storage
-    for (let i = 0; i < localStorage.length; i ++){
+    for (let i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i);
         var value = localStorage.getItem(key);
 
-        $(".time-zone").each(function(){
-            if ($(this).attr("id") == key){
+        $(".row").each(function () {
+            if ($(this).attr("id") == key) {
                 $(this).text(value);
             }
-        })
-    }
-})
+        });
+    };
+    
+});
